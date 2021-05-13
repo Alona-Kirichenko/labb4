f085531d374f35290ec49a9dad067d3d527da46a
alonakirichenkoivanovna <alena1412199217@gmail.com>
2021-05-13 12:52:05 +0200

    First commit 13Of may

---
+++ min-webbplats/precache-manifest.f0e1bcb9986e59dbd56722b8465e0e9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf3c10550990a2a7cb8",
+    "url": "/css/app.73c18ce0.css"
+  },
+  {
+    "revision": "c24f7e93a1dd195929773db2773c1e86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baf3c10550990a2a7cb8",
+    "url": "/js/app.7b48129b.js"
+  },
+  {
+    "revision": "b608f4c01c33ce7f2da8",
+    "url": "/js/chunk-vendors.a2a5a2a3.js"
+  },
+  {
+    "revision": "480271689585f326326dfdbcdc999222",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
